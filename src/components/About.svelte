9e2758cb32f29305daf11467fb0833d230891b51
Muhammad Saad <github.5s32y@slmail.me>
2023-11-22 21:23:02 +0000

    Add deployment workflow

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -11,7 +11,7 @@
     </div>
     <div class="card p-8 gap-4 max-w-sm md:max-w-md lg:max-w-xl xl:max-w-3xl bg-base-100 shadow-lg shadow-neutral hover:shadow-primary hover:scale-[102%] duration-300">
         <h2 class="card-title self-center text-4xl sm:text-5xl font-['The_Nautigal']">Muhammad Saad</h2>
-        <p class="text-justify text-sm sm:text-md">Using modern technologies to solve modern problems. My specialty is fullstack development but I love learning new technology. I'm passionate about privacy, security, cryptocurrency, productivity, fitness, and reading. If that sounds like you, check out my blog or reach out to connect.</p>
+        <p class="text-justify text-sm sm:text-md">Using modern technologies to solve modern problems. My specialty is fullstack development, but I love learning new technologies. I'm passionate about privacy, security, cryptocurrency, productivity, fitness, and reading. If that sounds like you, check out my blog or reach out to connect.</p>
         <div class="card-actions justify-end">
             <a class="btn btn-primary btn-xs sm:btn-sm" href="mailto:hi@mailias.maskmy.id" target="_blank" rel="noopener noreferrer">
                 Contact <span class="w-3 sm:w-4"><FaArrowRight/></span>
