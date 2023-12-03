fd87ebea3344077d3c39ab2c5a777c3974f9b47e
Muhammad Saad <github.5s32y@slmail.me>
2023-12-03 20:51:26 +0000

    Minor improvements

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,13 +3,13 @@
     import FaArrowRight from 'svelte-icons/fa/FaArrowRight.svelte'
 </script>
 
-<section class="p-8 flex flex-col items-center">
+<section class="p-8 w-scren flex flex-col items-center">
     <div class="flex items-center mb-6">
         <h2 class="flex gap-2 items-center text-lg sm:text-2xl md:text-3xl font-bold">
             <span class="h-4 w-4 sm:h-6 sm:w-6 md:h-8 md:w-8"><FaUserCircle/></span> About Me
         </h2>
     </div>
-    <div class="card p-8 gap-4 max-w-sm md:max-w-md lg:max-w-xl xl:max-w-3xl bg-base-100 shadow-lg shadow-neutral hover:shadow-primary hover:scale-[102%] duration-300">
+    <div class="card p-8 gap-4 w-[24rem] md:w-[28rem] lg:w-[42rem] bg-base-100 shadow-lg shadow-neutral hover:shadow-primary hover:scale-[102%] duration-300">
         <h2 class="card-title self-center text-4xl sm:text-5xl font-['The_Nautigal']">Muhammad Saad</h2>
         <p class="text-justify text-sm sm:text-md">Using modern technologies to solve modern problems. My specialty is fullstack development, but I love learning new technologies. I'm passionate about privacy, security, cryptocurrency, productivity, fitness, and reading. If that sounds like you, check out my blog or reach out to connect.</p>
         <div class="card-actions justify-end">
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,10 +2,12 @@
     import FaPaperPlane from 'svelte-icons/fa/FaPaperPlane.svelte'
     import FaGithub from 'svelte-icons/fa/FaGithub.svelte'
     import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte'
+
+    import hero from '../assets/hero.gif'
 </script>
 
 <section>
-    <div class="hero min-h-screen" style="background-image: url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9a45b024-f1d7-4024-b8ec-631c7bb458ba/dfzikmt-df170e0c-0a95-46f9-9ba5-d89c12a59329.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzlhNDViMDI0LWYxZDctNDAyNC1iOGVjLTYzMWM3YmI0NThiYVwvZGZ6aWttdC1kZjE3MGUwYy0wYTk1LTQ2ZjktOWJhNS1kODljMTJhNTkzMjkuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.JOZK4j0Z7NWnCNFVpKfN8DaBHfCHFWr-oNiiZeFiqKE);">
+    <div class="hero min-h-screen" style="background-image: url({hero});">
         <div class="hero-overlay bg-base-100 bg-opacity-50"></div>
         <div class="hero-content text-start">
           <div class="max-w-md md:max-w-lg lg:max-w-2xl px-4">
